72f355385283c29cd885732e9e2109c3181dd893
Andrew Wu <arw@dmp.com.tw>
2014-03-05 15:55:58 +0800

    Remove trailing space

--- parse_rtf.py
+++ parse_rtf.py
@@ -8,25 +8,25 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print "No input filename"
         sys.exit(1)
-    
+
     fn = sys.argv[1]
     print "Input file =", fn
-    
+
     # read file data
     f = open(fn, "rU")
     data = f.read()
-    
+
     # RTF last byte is \x00, remove it
     if data[-1] == '\x00':
         data = data[:-1]
-    
+
     print "data length = %d" % len(data)
-    
+
     '''
     # Give the lexer some input
     lex.input(data)
     #print data
-    
+
     # Tokenize
     print "token:"
     while 1:
@@ -34,17 +34,17 @@ if __name__ == "__main__":
         if not tok: break      # No more input
         print tok
     '''
-        
+
     #print data
     print "parse..."
     t1 = time.clock()
     res = rtf_parser.parse(data)
     t2 = time.clock()
     print "parse ok"
-        
+
     print "formating result text..."
     res_text = pprint.pformat(res)
-    
+
     #print
     #print res_text
     fo = open("result.txt", "wt")
